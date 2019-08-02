c2cfb3fc60e064ded123f47f509cb0a446ba06f5
cyprienjolivet <cyprien.jolivet@gmail.com>
2019-08-02 11:33:09 +0200

    Hello_moon

---
+++ R/hello_moon.R
@@ -0,0 +1,18 @@
+# Hello, moon!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, moon!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello_moon <- function() {
+  print("Hello, Moon!")
+}
